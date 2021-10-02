de9e831c0a4dddfa6e6de2837b90b2c02db19f82
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-10-02 10:55:05 +0800

    Rosemary

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
-DEVICE=aresin
-DT_LINK="https://github.com/mastersenpai0405/twrp_device_poco_F3_GT -b test"
-DT_PATH=device/poco/$DEVICE
+DEVICE=rosemary
+DT_LINK="https://github.com/mastersenpai05/twrp_device_xiomi_rosemary -b test"
+DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
