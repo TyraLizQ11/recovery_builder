66c0821dff46e7523995d0b12a488a6f60cb8469
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-10-20 22:20:18 +0800

    ares

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
-DEVICE=camellia
-DT_LINK="https://github.com/mastersenpai0405/twrp_device_xiaomi_camellia"
+DEVICE=ares
+DT_LINK="https://github.com/mastersenpai0405/recovery_device_xiaomi_ares"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
