d4e81e9c82cb54f84e06e686a2a8d0b6fb677d75
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-10-20 22:26:53 +0800

    juice

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
 DEVICE=juice
-DT_LINK="https://github.com/mastersenpai05/twrp_device_xiaomi_juice -b android-11"
+DT_LINK="https://github.com/mastersenpai05/twrp_device_xiaomi_juice -b android-11.0"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
