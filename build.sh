7f010b1b29ba67269e118ebdc415badeecf0fe43
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-11-26 16:51:47 +0800

    arestwrp

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/SHRP/platform_manifest_twrp_omni -b v3_11.0"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b v3_11.0"
 DEVICE=ares
-DT_LINK="https://github.com/mastersenpai0405/recovery_device_xiaomi_ares -b test"
+DT_LINK="https://github.com/mastersenpai0405/recovery_device_xiaomi_ares"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
