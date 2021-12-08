68a61175a63043222eaa84e3dd1bccaab304070c
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-12-08 20:36:19 +0800

    ATOM

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://gitlab.com/OrangeFox/sync.git"
-DEVICE=chopin
-DT_LINK="https://github.com/OrangeFoxUnOfficial/fox_device_xiaomi_chopin"
-DT_PATH=device/xiaomi/$DEVICE
+DEVICE=ATOM
+DT_LINK="https://github.com/OrangeFoxUnOfficial/fox_device_symphony_atom"
+DT_PATH=device/symphony/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -15,7 +15,7 @@ mkdir ~/twrp && cd ~/twrp
 
 echo " ===+++ Sync OrangeFox +++==="
 git clone $MANIFEST ~/FOX && cd ~/FOX
-./orangefox_sync.sh --branch 11.0 --path ~/fox_11.0
+./orangefox_sync.sh --branch 10.0 --path ~/fox_11.0
 cd ~/fox_11.0
 git clone $DT_LINK $DT_PATH
 
@@ -24,7 +24,7 @@ echo " ====+++ Building OrangeFox +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
-lunch twrp_${DEVICE}-eng && mka bootimage
+lunch twrp_${DEVICE}-eng && mka recoveryimage
 
 # Upload zips & recovery.img
 #echo " ===+++ Uploading Recovery +++===
