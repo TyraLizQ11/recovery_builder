573e52fb3443913c6174df683f59bb66a5057f4a
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2021-12-16 22:01:03 +0800

    X689

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/FoxRedd/platform_manifest_twrp_aosp"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
 DEVICE=X689
 DT_LINK="https://github.com/OrangeFoxUnOfficial/twrp_device_Infinix_X689"
 DT_PATH=device/infinix/$DEVICE
