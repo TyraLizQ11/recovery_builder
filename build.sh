d911649ee487c283777dd21e698a54b68987a893
Rommel Pangilinan <89135629+TyraLizQ11@users.noreply.github.com>
2022-05-30 19:53:43 +0800

    chopin

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://gitlab.com/OrangeFox/sync.git"
-DEVICE=X688B
-DT_LINK="https://github.com/OrangeFoxUnOfficial/recovery_device_infinix_X688B"
-DT_PATH=device/infinix/$DEVICE
+DEVICE=chopin
+DT_LINK="https://github.com/mastersenpai0405/fox_device_xiaomi_chopin"
+DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -15,8 +15,8 @@ mkdir ~/twrp && cd ~/twrp
 
 echo " ===+++ Sync OrangeFox +++==="
 git clone $MANIFEST ~/FOX && cd ~/FOX
-./orangefox_sync.sh --branch 10.0 --path ~/fox_10.0
-cd ~/fox_10.0
+./orangefox_sync.sh --branch 11.0 --path ~/fox_11.0
+cd ~/fox_11.0
 git clone $DT_LINK $DT_PATH
 
 echo " ====+++ Building OrangeFox +++==="
@@ -24,7 +24,7 @@ echo " ====+++ Building OrangeFox +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
-lunch omni_${DEVICE}-eng && mka recoveryimage
+lunch twrp_${DEVICE}-eng && mka bootimage
 
 # Upload zips & recovery.img
 #echo " ===+++ Uploading Recovery +++===
